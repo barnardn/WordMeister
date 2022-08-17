f64dfbac9867b8515ec42e13245897d3eae58ac3
Norm Barnard <norm@normbarnard.com>
2022-08-17 19:05:59 -0400

    Initial Commit

---
+++ WordMeister/WordMeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMeisterApp.swift
+//  WordMeister
+//
+//  Created by Norman Barnard on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordMeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
