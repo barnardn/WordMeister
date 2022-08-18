303054228c4b55a81a4d54583a9471eb207ea33a
Norm Barnard <norm@normbarnard.com>
2022-08-17 21:07:17 -0400

    added previews and initializers

--- WordmeisterLib/Sources/Models/DefinedWord.swift
+++ WordmeisterLib/Sources/Models/DefinedWord.swift
@@ -12,5 +12,26 @@ public struct DefinedWord: Identifiable, Codable, Equatable {
     public let word: String
     public let phonetics: [Phonetics]
     public let meanings: [Meaning]
-    public let sourceUrls: [URL]
+    public let sourceUrls: [URL?]?
+
+    internal init(word: String, phonetics: [Phonetics], meanings: [Meaning], sourceUrls: [URL?]?) {
+        self.word = word
+        self.phonetics = phonetics
+        self.meanings = meanings
+        self.sourceUrls = sourceUrls
+    }
+}
+
+extension DefinedWord {
+    public static var preview: DefinedWord {
+        DefinedWord(
+            word: "run",
+            phonetics: Phonetics.previews,
+            meanings: Meaning.previews,
+            sourceUrls: [
+                URL(string: "https://en.wiktionary.org/wiki/rin"),
+                URL(string: "https://en.wiktionary.org/wiki/riun")
+            ]
+        )
+    }
 }
--- WordmeisterLib/Sources/Models/License.swift
+++ WordmeisterLib/Sources/Models/License.swift
@@ -9,5 +9,19 @@ import Foundation
 
 public struct License: Codable, Equatable {
     public let name: String
-    public let url: URL
+    public let url: URL?
+
+    internal init(name: String, url: URL?) {
+        self.name = name
+        self.url = url
+    }
+}
+
+extension License {
+    public static var preview: License {
+        License(
+            name: "CC BY-SA 3.0",
+            url: URL(string: "https://creativecommons.org/licenses/by-sa/3.0")
+        )
+    }
 }
